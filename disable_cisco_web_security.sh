0f0e4e829c6170c578a87252bfa95deb1da27021
Steve Quirke <steve@quirke.org>
2014-05-01 17:02:50 +0100

    new Steve script files

---
+++ disable_cisco_web_security.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#Script to uninstall Cisco Websecurity  - add this to a workflow - and set it to run on firstboot
+
+#Remove the stupid Cisco WebSecurity module that we don't have a licence for, and that might cause browser issues
+
+./opt/cisco/anyconnect/bin/websecurity_uninstall.sh
+
+end 0
---
+++ enable_firewall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#Script to enable the firewall - add this to a workflow - and set it to run on firstboot
+
+#enable the OSX application firewall
+defaults write /Library/Preferences/com.apple.alf globalstate -int 1
+
+end 0
