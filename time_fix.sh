5f6ff90557b4816851884e2f7988db45ca22aace
Steve Quirke <steve@quirke.org>
2014-05-01 15:41:50 +0100

    added comments to explain what the script actually does

--- time_fix.sh
+++ time_fix.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#!/bin/sh
+#incredibly simple script to point the Mac being built at the local DC
+#new out of box machines often have a system clock a mile out - or at least out enough 
+#to prevent AD binding from working.
+#obviously change the DC to your local DC
 
 ntpdate londc01
 
